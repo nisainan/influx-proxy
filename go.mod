a0f8a099b48217ed7ed8d9de7d85343c481390b7
gitsrc <corerman@gmail.com>
2020-11-13 15:35:25 +0800

    feat(udp):upgrade performance by []byte pool and goPool

--- go.mod
+++ go.mod
@@ -12,8 +12,10 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c
 	github.com/panjf2000/ants/v2 v2.4.2
 	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.3.0 // indirect
--- service/udp.go
+++ service/udp.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: gitsrc
+ * @Date: 2020-11-10 19:24:27
+ * @LastEditors: gitsrc
+ * @LastEditTime: 2020-11-13 15:32:23
+ * @FilePath: /influx-proxy-gitsrc/service/udp.go
+ */
+
 package service
 
 import (
@@ -6,6 +14,8 @@ import (
 
 	"github.com/chengshiwen/influx-proxy/backend"
 	"github.com/chengshiwen/influx-proxy/transfer"
+	"github.com/oxtoacart/bpool"
+	"github.com/panjf2000/ants/v2"
 )
 
 //UDPService is UDP server
@@ -39,23 +49,47 @@ func (us *UDPService) ListenAndServe() (err error) {
 	defer pc.Close()
 
 	log.Printf("UDP service start on DB [%s], listen %s", us.UDPDatabase, us.UDPBind)
+	var bufpool *bpool.BytePool
+	// go func() {
+	// 	for {
+	// 		time.Sleep(time.Millisecond * 100)
+	// 		log.Println("count", bufpool.NumPooled())
+
+	// 	}
+	// }()
+
+	//create go pool
+	p, err := ants.NewPool(10000)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer p.Release() //release go pool
+
+	//create
+	bufpool = bpool.NewBytePool(125, 1024)
+
 	for {
-		buf := make([]byte, 1024)
+		buf := bufpool.Get() //内存池缓冲区获取
+
 		n, addr, err := pc.ReadFrom(buf)
 		if err != nil {
+			bufpool.Put(buf)
 			log.Println(err)
 			continue
 		}
-		go us.process(pc, addr, buf[:n])
+		ants.Submit(func() {
+			us.process(pc, addr, bufpool, buf, n) //内部包含内存释放
+		})
 	}
 }
 
-func (us *UDPService) process(pc net.PacketConn, addr net.Addr, buf []byte) {
-
+func (us *UDPService) process(pc net.PacketConn, addr net.Addr, bufpool *bpool.BytePool, buf []byte, n int) {
+	defer bufpool.Put(buf) //释放内存进入池中
 	if us.WriteTracing {
-		log.Printf("write: [%s %s]\n", us.UDPDatabase, buf)
+		log.Printf("write: [%s %s]\n", us.UDPDatabase, buf[:n])
 	}
-	err := us.ip.Write(buf, us.UDPDatabase, "ns")
+	err := us.ip.Write(buf[:n], us.UDPDatabase, "ns")
 	if err != nil {
 		log.Println(err)
 	}
